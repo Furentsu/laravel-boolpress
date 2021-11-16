ed045941b612cb2100df0205dd0b873becee6c99
Furentsu <eugeniofranzoso@gmail.com>
2021-11-16 17:21:47 +0100

    Modified both Post and Category controllers

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['class'];
+
+    public function posts() {
+        return $this->hasMany('App\Models\Post');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title','author','post_content','post_date','post_image'];
+
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
