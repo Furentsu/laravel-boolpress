c31ecbae38d68b620c74240f6115da548a8b6481
Furentsu <eugeniofranzoso@gmail.com>
2021-11-23 18:50:08 +0100

    Loader implemented in PostList component

--- resources/js/components/posts/PostList.vue
+++ resources/js/components/posts/PostList.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="container my-5">
         <h2>All Posts:</h2>
-        <PostCard v-for="(post,index) in posts" :key="index" :post="post"/>
+
+        <div class="my_loader" v-if="loading">
+            <div class="spinner-grow" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+
+        <PostCard v-else v-for="(post,index) in posts" :key="index" :post="post"/>
+        
     </div>
 </template>
 
@@ -18,18 +26,39 @@
             return {
                 posts: [],
                 localhost: 'http://127.0.0.1:8000',
+                loading: false
             }
         },
         created() {
+            this.loading = true;
+
             axios
             .get(`${this.localhost}/api/posts/`)
-            .then(response => (this.posts = response.data.posts) )
-            
+            .then(response => (this.posts = response.data.posts))
 
-            
             .catch((err) => {
                 console.error(err);
-            });
+            })
+
+            .then(()=> this.loading = false);
         }
     }
 </script>
+
+<style lang="scss" scoped>
+
+    .my_loader {
+        width: 100%;
+        position: fixed;
+        top : 0;
+        left : 0;
+        right : 0;
+        bottom : 0;
+        background-color: rgba(0,0,0,0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index : 8;
+}
+
+</style>
